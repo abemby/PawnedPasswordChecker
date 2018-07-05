5bd3b6da3d84a2363df2a5ab82e80962594ae924
ab <abemby@hotmail.com>
2018-07-05 20:21:37 +0100

    Add files via upload

---
+++ PawnedChecker/Form2.Designer.vb
@@ -0,0 +1,213 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class fPPChecker
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.clbPasswordsToCheck = New System.Windows.Forms.CheckedListBox()
+        Me.bLoad = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.tbWebApi = New System.Windows.Forms.TextBox()
+        Me.bVerify = New System.Windows.Forms.Button()
+        Me.bClear = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.tbFile = New System.Windows.Forms.TextBox()
+        Me.bSelect = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lStatus = New System.Windows.Forms.Label()
+        Me.bStop = New System.Windows.Forms.Button()
+        Me.bUnload = New System.Windows.Forms.Button()
+        Me.bVerify2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'clbPasswordsToCheck
+        '
+        Me.clbPasswordsToCheck.Enabled = False
+        Me.clbPasswordsToCheck.FormattingEnabled = True
+        Me.clbPasswordsToCheck.Location = New System.Drawing.Point(12, 94)
+        Me.clbPasswordsToCheck.Name = "clbPasswordsToCheck"
+        Me.clbPasswordsToCheck.Size = New System.Drawing.Size(445, 424)
+        Me.clbPasswordsToCheck.TabIndex = 0
+        '
+        'bLoad
+        '
+        Me.bLoad.Location = New System.Drawing.Point(152, 65)
+        Me.bLoad.Name = "bLoad"
+        Me.bLoad.Size = New System.Drawing.Size(52, 23)
+        Me.bLoad.TabIndex = 1
+        Me.bLoad.Text = "Load"
+        Me.bLoad.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(11, 42)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(50, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Web API"
+        '
+        'tbWebApi
+        '
+        Me.tbWebApi.Location = New System.Drawing.Point(62, 39)
+        Me.tbWebApi.Name = "tbWebApi"
+        Me.tbWebApi.ReadOnly = True
+        Me.tbWebApi.Size = New System.Drawing.Size(398, 20)
+        Me.tbWebApi.TabIndex = 3
+        Me.tbWebApi.Text = "https://api.pwnedpasswords.com/range/{0}"
+        '
+        'bVerify
+        '
+        Me.bVerify.Location = New System.Drawing.Point(303, 65)
+        Me.bVerify.Name = "bVerify"
+        Me.bVerify.Size = New System.Drawing.Size(52, 23)
+        Me.bVerify.TabIndex = 4
+        Me.bVerify.Text = "Verify"
+        Me.bVerify.UseVisualStyleBackColor = True
+        '
+        'bClear
+        '
+        Me.bClear.Location = New System.Drawing.Point(256, 65)
+        Me.bClear.Name = "bClear"
+        Me.bClear.Size = New System.Drawing.Size(47, 23)
+        Me.bClear.TabIndex = 5
+        Me.bClear.Text = "Clear"
+        Me.bClear.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 15)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(23, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "File"
+        '
+        'tbFile
+        '
+        Me.tbFile.Location = New System.Drawing.Point(62, 12)
+        Me.tbFile.Name = "tbFile"
+        Me.tbFile.Size = New System.Drawing.Size(366, 20)
+        Me.tbFile.TabIndex = 7
+        '
+        'bSelect
+        '
+        Me.bSelect.Location = New System.Drawing.Point(436, 10)
+        Me.bSelect.Name = "bSelect"
+        Me.bSelect.Size = New System.Drawing.Size(24, 23)
+        Me.bSelect.TabIndex = 8
+        Me.bSelect.Text = "..."
+        Me.bSelect.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 75)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(123, 13)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Are Password Pawned ?"
+        '
+        'lStatus
+        '
+        Me.lStatus.AutoSize = True
+        Me.lStatus.Location = New System.Drawing.Point(12, 521)
+        Me.lStatus.Name = "lStatus"
+        Me.lStatus.Size = New System.Drawing.Size(16, 13)
+        Me.lStatus.TabIndex = 10
+        Me.lStatus.Text = "..."
+        '
+        'bStop
+        '
+        Me.bStop.Enabled = False
+        Me.bStop.Location = New System.Drawing.Point(407, 65)
+        Me.bStop.Name = "bStop"
+        Me.bStop.Size = New System.Drawing.Size(53, 23)
+        Me.bStop.TabIndex = 11
+        Me.bStop.Text = "Stop"
+        Me.bStop.UseVisualStyleBackColor = True
+        '
+        'bUnload
+        '
+        Me.bUnload.Location = New System.Drawing.Point(204, 65)
+        Me.bUnload.Name = "bUnload"
+        Me.bUnload.Size = New System.Drawing.Size(52, 23)
+        Me.bUnload.TabIndex = 2
+        Me.bUnload.Text = "Unload"
+        Me.bUnload.UseVisualStyleBackColor = True
+        '
+        'bVerify2
+        '
+        Me.bVerify2.Location = New System.Drawing.Point(355, 65)
+        Me.bVerify2.Name = "bVerify2"
+        Me.bVerify2.Size = New System.Drawing.Size(52, 23)
+        Me.bVerify2.TabIndex = 12
+        Me.bVerify2.Text = "Verify 2"
+        Me.bVerify2.UseVisualStyleBackColor = True
+        '
+        'fPPChecker
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.ClientSize = New System.Drawing.Size(471, 541)
+        Me.Controls.Add(Me.bVerify2)
+        Me.Controls.Add(Me.bUnload)
+        Me.Controls.Add(Me.bStop)
+        Me.Controls.Add(Me.lStatus)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.bSelect)
+        Me.Controls.Add(Me.tbFile)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.bClear)
+        Me.Controls.Add(Me.bVerify)
+        Me.Controls.Add(Me.tbWebApi)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.bLoad)
+        Me.Controls.Add(Me.clbPasswordsToCheck)
+        Me.DoubleBuffered = True
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "fPPChecker"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Pawned Password Checker"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents clbPasswordsToCheck As CheckedListBox
+    Friend WithEvents bLoad As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents tbWebApi As TextBox
+    Friend WithEvents bVerify As Button
+    Friend WithEvents bClear As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents tbFile As TextBox
+    Friend WithEvents bSelect As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lStatus As Label
+    Friend WithEvents bStop As Button
+    Friend WithEvents bUnload As Button
+    Friend WithEvents bVerify2 As Button
+End Class
---
+++ PawnedChecker/Form2.vb
@@ -0,0 +1,349 @@
+﻿Imports System.Configuration
+Imports System.IO
+Imports System.Net
+Imports System.Net.Http
+
+Public Class fPPChecker
+
+#Region "Events"
+    Private Sub fPPChecker_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lStatus.Text = "fPPChecker_Load"
+        Try
+            ReadConfig()
+            Initialise()
+
+        Catch ex As Exception
+            lStatus.Text = String.Format("fPPChecker_Load Error: {0}", ex.Message)
+        End Try
+
+    End Sub
+
+
+    Private Sub bSelect_Click(sender As Object, e As EventArgs) Handles bSelect.Click
+        lStatus.Text = "bSelect_Click"
+        Try
+            SelectFile()
+        Catch ex As Exception
+            lStatus.Text = String.Format("bSelect_Click Error: {0}", ex.Message)
+        End Try
+
+    End Sub
+
+
+    Private Sub bLoad_Click(sender As Object, e As EventArgs) Handles bLoad.Click
+        lStatus.Text = "bLoad_Click"
+        Try
+            If Not String.IsNullOrEmpty(tbFile.Text) Then
+                Dim dataSource = File.ReadAllLines(tbFile.Text).Distinct().ToList()
+
+                clbPasswordsToCheck.DataSource = dataSource
+                bVerify.Enabled = (clbPasswordsToCheck.Items.Count > 0)
+                bVerify2.Enabled = bVerify.Enabled
+            End If
+
+        Catch ex As Exception
+            lStatus.Text = String.Format("bLoad_Click Error: {0}", ex.Message)
+        End Try
+
+    End Sub
+
+
+    Private Sub bUnload_Click(sender As Object, e As EventArgs) Handles bUnload.Click
+        lStatus.Text = "bUnload_Click"
+        Try
+            Initialise()
+        Catch ex As Exception
+            lStatus.Text = String.Format("bUnload_Click Error: {0}", ex.Message)
+        End Try
+    End Sub
+
+
+    Private Sub bClear_Click(sender As Object, e As EventArgs) Handles bClear.Click
+        lStatus.Text = "bClear_Click"
+        Try
+            UnCheckAll(clbPasswordsToCheck)
+        Catch ex As Exception
+            lStatus.Text = String.Format("bClear_Click Error: {0}", ex.Message)
+        End Try
+    End Sub
+
+    Private Sub bVerify_Click(sender As Object, e As EventArgs) Handles bVerify.Click
+        lStatus.Text = "bVerify_Click"
+        Try
+            If clbPasswordsToCheck.Items.Count > 0 Then
+                bStop.Enabled = True
+
+                For counter As Integer = 0 To clbPasswordsToCheck.Items.Count - 1
+                    If Not clbPasswordsToCheck.GetItemChecked(counter) Then
+
+                        Dim passwordToCheck = clbPasswordsToCheck.Items(counter).ToString()
+
+                        'check if password is pawned
+                        If Not String.IsNullOrEmpty(passwordToCheck) Then
+                            If IsPasswordPawned(passwordToCheck) Then
+                                clbPasswordsToCheck.SetItemCheckState(counter, CheckState.Checked)
+                            End If
+                        End If
+
+                    End If
+                Next
+
+                bStop.Enabled = False
+            End If
+
+            'lStatus.Text = "Done"
+        Catch ex As Exception
+            lStatus.Text = String.Format("bVerify_Click Error: {0}", ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub bVerify2_Click(sender As Object, e As EventArgs) Handles bVerify2.Click
+        lStatus.Text = "bVerify2_Click"
+        Try
+            If clbPasswordsToCheck.Items.Count > 0 Then
+                bStop.Enabled = True
+
+                For counter As Integer = 0 To clbPasswordsToCheck.Items.Count - 1
+                    If Not clbPasswordsToCheck.GetItemChecked(counter) Then
+
+                        Dim passwordToCheck = clbPasswordsToCheck.Items(counter).ToString()
+
+                        'check if password is pawned
+                        If Not String.IsNullOrEmpty(passwordToCheck) Then
+                            If IsPasswordPawnedV2(passwordToCheck) Then
+                                clbPasswordsToCheck.SetItemCheckState(counter, CheckState.Checked)
+                            End If
+                        End If
+
+                    End If
+                Next
+
+                bStop.Enabled = False
+            End If
+
+            lStatus.Text = "Done"
+        Catch ex As Exception
+            lStatus.Text = String.Format("bVerify2_Click Error: {0}", ex.Message)
+        End Try
+    End Sub
+
+    Private Sub bStop_Click(sender As Object, e As EventArgs) Handles bStop.Click
+        lStatus.Text = "SelectFile"
+        bStop.Enabled = False
+    End Sub
+
+
+#End Region
+
+#Region "Functions/Methods"
+    Private Sub ReadConfig()
+        Dim toolTip As New ToolTip()
+        toolTip.ShowAlways = True
+
+        tbWebApi.Text = ConfigurationSettings.AppSettings("webapi").ToString()
+        tbWebApi.Tag = ConfigurationSettings.AppSettings("firstn").ToString()
+
+        clbPasswordsToCheck.Tag = ConfigurationSettings.AppSettings("timeout").ToString()
+        If Not IsNumeric(clbPasswordsToCheck.Tag) Then
+            clbPasswordsToCheck.Tag = "5000000"
+        End If
+
+        toolTip.SetToolTip(tbWebApi, tbWebApi.Tag)
+    End Sub
+
+    Private Sub Initialise()
+
+        Dim pwdFileName = ConfigurationSettings.AppSettings("PwdFileName").ToString()
+        Dim rootPath As String = Application.StartupPath.ToLower.Replace("\bin\debug", "").Replace("\bin\release", "")
+        tbFile.Text = String.Format("{0}\{1}", rootPath, pwdFileName)
+
+        clbPasswordsToCheck.DataSource = Nothing
+        clbPasswordsToCheck.Items.Clear()
+
+        bVerify.Enabled = False
+        bVerify2.Enabled = False
+        bStop.Enabled = False
+    End Sub
+
+    Private Sub SelectFile()
+        lStatus.Text = "SelectFile"
+
+        Dim rootPath As String = Application.StartupPath.ToLower.Replace("\bin\debug", "").Replace("\bin\release", "")
+        'rootPath = String.Format("{0}\TextFile1.txt", rootPath)
+
+
+        Dim fd As OpenFileDialog = New OpenFileDialog()
+
+        fd.Title = "Open File Dialog"
+        fd.InitialDirectory = rootPath
+        'fd.Filter = "All files (*.*)|*.*|All files (*.*)|*.*"
+        fd.Filter = "Text file|*.txt"
+        fd.FilterIndex = 2
+        fd.RestoreDirectory = True
+
+        If fd.ShowDialog() = DialogResult.OK Then
+            tbFile.Text = fd.FileName
+        End If
+    End Sub
+
+    Private Sub UnCheckAll(clbPasswordsToCheck As CheckedListBox)
+        For counter As Integer = 0 To clbPasswordsToCheck.Items.Count - 1
+            clbPasswordsToCheck.SetItemChecked(counter, False)
+        Next
+    End Sub
+
+    Private Function IsPasswordPawned(passwordToCheck As String) As Boolean
+        lStatus.Text = "IsPasswordPawned"
+
+        Dim bPawned As Boolean = False
+
+        Try
+
+
+            Dim sha1 = getSHA1Hash(passwordToCheck)
+
+            'get first 5
+            Dim hash5 = Mid(sha1, 1, 5)
+
+            'form webapi
+            Dim webAPI = tbWebApi.Text.Replace("{0}", hash5)
+
+            'get list of pawned password from httpclient
+            'Using client As New WebClient()
+
+            'webclient with timeout....
+            Using client As New WebClientWithTimeout(New TimeSpan(Integer.Parse(clbPasswordsToCheck.Tag.ToString())))
+
+                lStatus.Text = "WebClient_DownloadString"
+
+                Dim apiData As String = client.DownloadString(webAPI)
+
+                Dim parts As List(Of String) = apiData.Split(New String() {Environment.NewLine}, StringSplitOptions.None).ToList()
+
+                For Each part As String In parts
+                    Application.DoEvents()
+
+                    Dim partSplit = part.Split(New Char() {":"c})
+
+                    Dim partSplitHash5 = String.Format("{0}{1}", hash5, partSplit(0))
+                    Dim partSplitHash5Count = partSplit(1)
+
+                    lStatus.Text = partSplitHash5
+                    If partSplitHash5.ToLower() = sha1.ToLower() Then
+                        bPawned = True
+                        Exit For
+                    End If
+
+                    If Not bStop.Enabled Then
+                        Exit For
+                    End If
+
+                Next
+
+            End Using
+
+
+        Catch ex As Exception
+            lStatus.Text = String.Format("IsPasswordPawned Error: {0}", ex.Message)
+        End Try
+
+        Return bPawned
+
+    End Function
+
+    Private Function IsPasswordPawnedV2(passwordToCheck As String) As Boolean
+        lStatus.Text = "IsPasswordPawnedV2"
+
+        Dim bPawned As Boolean = False
+
+        Try
+
+            Dim sha1 = getSHA1Hash(passwordToCheck)
+
+            'get first 5
+            Dim hash5 = Mid(sha1, 1, 5)
+
+            'form webapi
+            Dim uri = tbWebApi.Text.Replace("{0}", hash5)
+
+            Dim httpClient = New HttpClient()
+
+            httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
+
+            Dim response = httpClient.GetStringAsync(New Uri(uri)).Result
+
+            Dim parts As List(Of String) = response.Split(New String() {Environment.NewLine}, StringSplitOptions.None).ToList()
+
+            For Each part As String In parts
+                Application.DoEvents()
+
+                Dim partSplit = part.Split(New Char() {":"c})
+
+                Dim partSplitHash5 = String.Format("{0}{1}", hash5, partSplit(0))
+                Dim partSplitHash5Count = partSplit(1)
+
+                lStatus.Text = partSplitHash5
+                If partSplitHash5.ToLower() = sha1.ToLower() Then
+                    bPawned = True
+                    Exit For
+                End If
+
+                If Not bStop.Enabled Then
+                    Exit For
+                End If
+
+            Next
+
+            httpClient.Dispose()
+
+        Catch ex As Exception
+            lStatus.Text = String.Format("IsPasswordPawnedV2 Error: {0}", ex.Message)
+        End Try
+
+        Return bPawned
+    End Function
+
+    Function getSHA1Hash(ByVal strToHash As String) As String
+        lStatus.Text = "getSHA1Hash"
+
+        Dim sha1Obj As New System.Security.Cryptography.SHA1CryptoServiceProvider
+        Dim bytesToHash() As Byte = System.Text.Encoding.ASCII.GetBytes(strToHash)
+
+        bytesToHash = sha1Obj.ComputeHash(bytesToHash)
+
+        Dim strResult As String = ""
+
+        For Each b As Byte In bytesToHash
+            strResult += b.ToString("x2")
+        Next
+
+        Return strResult
+
+    End Function
+
+#End Region
+
+
+End Class
+
+
+Public Class WebClientWithTimeout
+    Inherits WebClient
+
+    Private ReadOnly _timeSpan As TimeSpan
+
+    Public Sub New()
+        _timeSpan = New TimeSpan(5000000)
+    End Sub
+
+    Public Sub New(timeSpan As TimeSpan)
+        _timeSpan = timeSpan
+    End Sub
+
+    Protected Overrides Function GetWebRequest(address As Uri) As WebRequest
+        Dim wr As WebRequest = MyBase.GetWebRequest(address)
+        wr.Timeout = _timeSpan.Milliseconds ' // timeout in milliseconds (ms)
+        Return wr
+    End Function
+End Class
